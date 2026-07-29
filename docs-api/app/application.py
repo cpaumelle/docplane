a7708a8536d368beb089ee08fd90f9366f97a6bc
cpaumelle2 <chpa35@gmail.com>
2026-07-29 11:08:57 +0200

    api: install operation contract surface

--- docs-api/app/application.py
+++ docs-api/app/application.py
@@ -7,6 +7,7 @@ from app.agent_contract_api import REPLACED_AGENT_PATHS, install_agent_contract,
 from app.agent_shortcuts_api import router as agent_shortcuts_router
 from app.event_api import router as event_router
 from app.http_semantics import HeadAsGetMiddleware
+from app.operation_contract_api import install_operation_contract, router as operation_contract_router
 from app.reorganisation_api import router as reorganisation_router
 from app.system_api import router as system_router
 from app.trust_api import router as trust_router
@@ -23,6 +24,7 @@ app = FastAPI(
 app.add_middleware(HeadAsGetMiddleware)
 app.include_router(system_router)
 app.include_router(agent_contract_router)
+app.include_router(operation_contract_router)
 app.include_router(agent_access_router)
 app.include_router(agent_router)
 app.include_router(agent_shortcuts_router)
@@ -31,5 +33,6 @@ app.include_router(work_router)
 app.include_router(trust_router)
 app.include_router(reorganisation_router)
 install_agent_contract(app)
+install_operation_contract(app)
 
 __all__ = ["app"]
